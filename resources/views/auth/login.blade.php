b8997bea5be88cac4f9db4fe773c010ea1be4075
unknown <wiamprawira24@gmail.com>
2026-05-29 20:53:29 +0700

    tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - SIMINAT</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @fluxStyles
 </head>
 <body class="bg-white min-h-screen font-sans">
     
